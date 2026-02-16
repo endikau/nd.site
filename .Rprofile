d9b55701c52bf508b38dd847a98e1293ea275659
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2026-02-16 16:31:58 +0100

    update

--- .Rprofile
+++ .Rprofile
@@ -1,3 +1,11 @@
-source("renv/activate.R")
+options(
+  renv.config.pak.enabled = TRUE,
+  renv.config.ppm.enabled = TRUE,
+  renv.config.ppm.default = TRUE,
+  repos = c(
+    P3M_MANYLINUX = "https://p3m.dev/cran/__linux__/manylinux_2_28/latest", 
+    getOption("repos")
+  )
+)
 
-Sys.setlocale("LC_ALL", "de_DE.UTF-8")
+source("renv/activate.R")
--- scripts/setup_envs.R
+++ scripts/setup_envs.R
@@ -1,58 +1,71 @@
 install.packages(
   pkgs = setdiff(
-    c("renv", "callr", "cli"), 
+    c("renv", "callr", "cli", "pak"),
     rownames(installed.packages())
   ),
   repos = "https://cloud.r-project.org"
 )
 
-callr::r(\(...){
-  options(
-    renv.config.ppm.enabled = TRUE,
-    renv.config.ppm.default = TRUE,
-    renv.config.ppm.url = "https://packagemanager.posit.co/cran/latest"
-  )
-  renv::activate()
-})
+callr::r(
+  \(...) {
+    renv::activate()
+  },
+  show = TRUE
+)
 
-callr::r(\(...){
-  options(
-    renv.config.ppm.enabled = TRUE,
-    renv.config.ppm.default = TRUE,
-    renv.config.ppm.url = "https://packagemanager.posit.co/cran/latest"
-  )
-  renv::restore()
-})
+callr::r(
+  \(...) {
+    renv::restore()
+  },
+  show = TRUE
+)
 
-callr::r(\(...){
-  renv::install(c("yaml", "reticulate", "m-pilarski/helprrr"), prompt = FALSE)
-})
+callr::r(
+  \(...) {
+    renv::install(
+      c("yaml", "reticulate", "m-pilarski/helprrr"),
+      prompt = FALSE
+    )
+  },
+  show = TRUE
+)
 
 cli::cli_alert_success("renv")
 
-callr::r(\(...){
-  .python_pyenv_path <- reticulate::install_python("3.12:latest", force = FALSE)
-  if (!reticulate::virtualenv_exists("./venv")) { 
-    reticulate::virtualenv_create(
-      envname = "./venv",
-      python = .python_pyenv_path,
-      requiremens = (if (fs::file_exists("requirements.txt")) {
-        "requirements.txt"
-      } else {
-        NULL
-      })
+callr::r(
+  \(...) {
+    .python_pyenv_path <- reticulate::install_python(
+      "3.12:latest",
+      force = FALSE
     )
-  }
-  .python_venv_path <- reticulate::virtualenv_python("./venv")
-  renv::use_python(.python_venv_path, type = "virtualenv")
-  helprrr::setenv_persist(
-    RETICULATE_PYTHON = .python_venv_path,
-    RENV_PYTHON = .python_venv_path
-  )
-})
+    if (!reticulate::virtualenv_exists("./venv")) {
+      reticulate::virtualenv_create(
+        envname = "./venv",
+        python = .python_pyenv_path,
+        requiremens = (if (fs::file_exists("requirements.txt")) {
+          "requirements.txt"
+        } else {
+          NULL
+        })
+      )
+    }
+    .python_venv_path <- reticulate::virtualenv_python("./venv")
+    renv::use_python(.python_venv_path, type = "virtualenv")
+    helprrr::setenv_persist(
+      RETICULATE_PYTHON = .python_venv_path,
+      RENV_PYTHON = .python_venv_path
+    )
+  },
+  show = TRUE
+)
 
 cli::cli_alert_success("venv")
 
-callr::r(\(...){renv::snapshot(prompt = FALSE)})
+callr::r(
+  \(...) {
+    renv::snapshot(prompt = FALSE)
+  },
+  show = TRUE
+)
 
 cli::cli_alert_success("snapshot")
